f456fca6b121ea293510cbc5aa43e668703d7b98
Abdullahyanik0 <95146820+Abdullahyanik0@users.noreply.github.com>
2022-06-23 03:18:47 +0300

    Update CharacterItem.jsx

--- src/components/CharacterItem.jsx
+++ src/components/CharacterItem.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const CharacterItem = ({ item }) => {
   return (
-    <div className=" w-[220px] h-[300px]  shadow-2xl shadow-red-700 hover:shadow-red-400">
+    <div className=" w-[220px] h-[300px] lg:w-[150px] lg:h-[240px] rounded-md  shadow-2xl shadow-red-700 hover:shadow-red-400">
       <img
         src={item.thumbnail.path + "/portrait_xlarge.jpg"}
         alt=""
-        className="w-[220px] h-[260px] object-cover rounded-md "
+        className="w-[220px] h-[260px] object-cover rounded-md lg:w-[150px] lg:h-[200px] "
       />
       <h1 className="flex justify-center py-2 ">{item.name}</h1>
     </div>
